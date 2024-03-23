f6e7d8c95a7dd3ed08067054461c24eb0efd4734
Saransh05 <123106127+Saransh05@users.noreply.github.com>
2024-03-23 18:45:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int K) {
+        //Complete the function
+        HashMap<Integer,Integer> h=new HashMap<>();
+        int presum=0;
+        int length=0;
+        for(int i=0;i<N;i++)
+        {
+            presum=presum+A[i];
+           if(presum==K)
+           {
+               length=i+1;
+           }
+            if(h.containsKey(presum-K))
+            {
+                 length=Math.max(length,i-h.get(presum-K));
+            }
+            if(!h.containsKey(presum)){
+                h.put(presum,i);
+            }
+        }
+        return length;
+    }
+    
+    
+}
+
+
