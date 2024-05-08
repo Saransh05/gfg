86733c03725c7ba274f639fc7233c9a845c7bf68
Saransh Kapoor <123106127+Saransh05@users.noreply.github.com>
2024-05-08 19:19:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Maximum of all subarrays of size k/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+//User function template for JAVA
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        Deque<Integer> dq=new LinkedList<>();
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int i=0;i<k;i++)
+        {
+            while(!dq.isEmpty() && arr[i]>=arr[dq.peekLast()])
+            {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+        }
+        for(int i=k;i<n;i++)
+        {
+          al.add(arr[dq.peekFirst()]);
+          while(!dq.isEmpty() && dq.peekFirst()<=(i-k))
+          {
+              dq.pollFirst();
+          }
+          while(!dq.isEmpty() && arr[i]>=arr[dq.peekLast()])
+          {
+            dq.pollLast();
+          }
+          dq.offerLast(i);
+        }
+        al.add(arr[dq.peekFirst()]);
+        return al;
+    }
+}
